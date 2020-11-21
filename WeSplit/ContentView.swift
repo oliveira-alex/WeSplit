3d79b2cc7923f1faf204e51b29a1f7b3470be305
Alex Oliveira <alexdeoliveira.poli@gmail.com>
2020-11-21 11:59:39 -0300

    End of Part 2 - Creating a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		Form {
+			Section {
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+			}
+			
+			Section {
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+				Text("Hello, world!")
+			}
+		}
     }
 }
 
